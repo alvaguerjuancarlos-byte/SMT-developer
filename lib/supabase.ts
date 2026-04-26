e785ad5a789b902501cfd8bdcc27ae34f5a4583f
Juan Carlos Alvarez <jcalvarez@mindbridge.com.mx>
2026-04-26 09:24:28 -0600

    Supabase con variables de entorno

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://blfuuouexpzcnqpfjhaw.supabase.co'
-const supabaseKey = 'sb_publishable_GQmlLQLhrQ5zALvahk5D2g_eaAcDZ0m'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
